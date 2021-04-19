32ffab42f75ac71a89f8e3c5fcd4d6e747772612
mahamqiam14 <maham.m.qiam@gmail.com>
2021-04-19 07:14:55 +0500

    added cartIcon and cartDropdown components to application, implemented cartReducer

--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,3 +1,4 @@
+import { UserActionTypes } from './user.types'
 
 const INITIAL_STATE = {
 currrentUser: null
@@ -5,7 +6,7 @@ currrentUser: null
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'SET_CURRENT_USER':
+        case UserActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: action.payload
